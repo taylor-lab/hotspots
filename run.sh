77770c711428154d033c7971e3490ea7e44dbfbd
dchakro <35454738+dchakro@users.noreply.github.com>
2021-03-04 12:26:46 +0200

    shell script to run

---
+++ run.sh
@@ -0,0 +1,13 @@
+./hotspot_algo.R \
+	--input-maf=ERBBs/Data/ERBBS.maf \
+	--rdata=hotspot_algo.Rdata \
+	--output-file=ERBBs/sig_hotspots.txt \
+	--homopolymer=FALSE
+
+
+# ./hotspot_algo.R \
+# 	--input-maf=ERBBs/Data/ERBBS.maf \
+# 	--rdata=hotspot_algo.Rdata \
+# 	--gene-query=ERBBs/Data/genes.txt \
+# 	--output-file=sig_hotspots.txt \
+# 	--homopolymer=FALSE
